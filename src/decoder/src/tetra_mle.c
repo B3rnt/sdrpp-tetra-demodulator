ea0f6505988ba2572779cbc01abb02e42fd594b0
B3rnt <36112072+B3rnt@users.noreply.github.com>
2025-12-12 23:58:59 +0200

    Update tetra_mle.c
    ISSI

--- src/decoder/src/tetra_mle.c
+++ src/decoder/src/tetra_mle.c
@@ -25,27 +25,56 @@ int rx_tl_sdu(struct tetra_mac_state *tms, struct msgb *msg, unsigned int len)
     uint8_t *bits = msg->l3h;
     uint8_t mle_pdisc = bits_to_uint(bits, 3);
 
-    // debug: laat zien welke PDISC je überhaupt krijgt
+    /* Log altijd PDISC + SSI context */
     {
-        char b[64];
-        snprintf(b, sizeof(b), "MLE PDISC=%u (%s)", mle_pdisc, tetra_get_mle_pdisc_name(mle_pdisc));
+        char b[128];
+
+        if (tms && tms->ssi) {
+            snprintf(b, sizeof(b),
+                     "ISSI=%u (0x%06X) MLE PDISC=%u (%s)",
+                     (unsigned)tms->ssi, (unsigned)tms->ssi,
+                     (unsigned)mle_pdisc, tetra_get_mle_pdisc_name(mle_pdisc));
+        } else {
+            snprintf(b, sizeof(b),
+                     "ISSI=UNKNOWN MLE PDISC=%u (%s)",
+                     (unsigned)mle_pdisc, tetra_get_mle_pdisc_name(mle_pdisc));
+        }
+
         mm_log(b);
     }
 
     switch (mle_pdisc) {
-    case TMLE_PDISC_MM:
-        tetra_get_mm_pdut_name(bits_to_uint(bits+3, 4), 0);
+    case TMLE_PDISC_MM: {
+        /* MM PDU type is 4 bits starting at bit 3 */
+        uint8_t mm_type = bits_to_uint(bits + 3, 4);
+        const char *mm_name = tetra_get_mm_pdut_name(mm_type, 0);
+
+        char b[160];
+        if (tms && tms->ssi) {
+            snprintf(b, sizeof(b),
+                     "ISSI=%u (0x%06X) MM type=0x%X (%s)",
+                     (unsigned)tms->ssi, (unsigned)tms->ssi,
+                     (unsigned)mm_type, mm_name);
+        } else {
+            snprintf(b, sizeof(b),
+                     "ISSI=UNKNOWN MM type=0x%X (%s)",
+                     (unsigned)mm_type, mm_name);
+        }
+        mm_log(b);
         break;
+    }
+
     case TMLE_PDISC_CMCE:
-        // eventueel ook loggen
-        // tetra_get_cmce_pdut_name(bits_to_uint(bits+3, 5), 0);
+        /* optioneel */
         break;
+
     case TMLE_PDISC_SNDCP:
-        // dit bevestigt “ik zat op een data kanaal”
-        // mm_log("SNDCP TL-SDU");
+        /* optioneel */
         break;
+
     default:
         break;
     }
+
     return len;
 }
