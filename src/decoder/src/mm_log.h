d431ffe489e5b0033083b0a021d865ad61a21ee1
B3rnt <36112072+B3rnt@users.noreply.github.com>
2025-12-12 23:29:45 +0200

    Update mm_log.h
    MM

--- src/decoder/src/mm_log.h
+++ src/decoder/src/mm_log.h
@@ -1,11 +1,12 @@
 #pragma once
-#include <stdio.h>
 
-/* simpele append-logger voor MM PDUs */
-static inline void mm_log(const char *line)
-{
-    FILE *f = fopen("tetra_mm.log", "a");
-    if (!f) return;
-    fprintf(f, "%s\n", line);
-    fclose(f);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Simple file logger for MM/MLE debug */
+void mm_log(const char *line);
+
+#ifdef __cplusplus
 }
+#endif
