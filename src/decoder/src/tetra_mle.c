b2ae6bc0fb1d56b928319c685a663df562ec53ec
B3rnt <36112072+B3rnt@users.noreply.github.com>
2025-12-14 19:18:55 +0200

    Update tetra_mle.c
    fix

--- src/decoder/src/tetra_mle.c
+++ src/decoder/src/tetra_mle.c
@@ -1,11 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
-// #include <unistd.h>
 #include <string.h>
-
-// #include <osmocom/core/msgb.h>
-// #include <osmocom/core/talloc.h>
-// #include <osmocom/core/bits.h>
 
 #include "tetra_mle_pdu.h"
 #include "tetra_mle.h"
@@ -15,11 +10,7 @@
 #include "tetra_sndcp_pdu.h"
 #include "tetra_mle_pdu.h"
 
-
-//TODO: stole D-* parser from sq5bpf
-
 /* Receive TL-SDU (LLC SDU == MLE PDU) */
-
 int rx_tl_sdu(struct tetra_mac_state *tms, struct msgb *msg, unsigned int len)
 {
     uint8_t *bits = msg->l3h;
@@ -27,20 +18,19 @@ int rx_tl_sdu(struct tetra_mac_state *tms, struct msgb *msg, unsigned int len)
 
     /* Log altijd PDISC + SSI context */
     {
-        char b[128];
-
         if (tms && tms->ssi) {
-            snprintf(b, sizeof(b),
-                     "ISSI=%u (0x%06X) MLE PDISC=%u (%s)",
-                     (unsigned)tms->ssi, (unsigned)tms->ssi,
-                     (unsigned)mle_pdisc, tetra_get_mle_pdisc_name(mle_pdisc));
-        } else {
-            snprintf(b, sizeof(b),
-                     "ISSI=UNKNOWN MLE PDISC=%u (%s)",
-                     (unsigned)mle_pdisc, tetra_get_mle_pdisc_name(mle_pdisc));
-        }
+            // LA ophalen (bijvoorbeeld van de decoder of uit context)
+            int la = _this->osmotetradecoder.getLA(); // Zorg ervoor dat la echt wordt ingevuld
 
-        mm_log(b);
+            // Log MLE PDISC met echte ISSI
+            mm_logf_with_ctx(tms->ssi, la, "MLE PDISC=%u (%s) ISSI=%u (0x%06X)",
+                             (unsigned)mle_pdisc, tetra_get_mle_pdisc_name(mle_pdisc),
+                             (unsigned)tms->ssi, (unsigned)tms->ssi);
+        } else {
+            // Als ISSI onbekend is
+            mm_logf_with_ctx(0, -1, "MLE PDISC=%u (%s) ISSI=UNKNOWN",
+                             (unsigned)mle_pdisc, tetra_get_mle_pdisc_name(mle_pdisc));
+        }
     }
 
     switch (mle_pdisc) {
@@ -49,27 +39,28 @@ int rx_tl_sdu(struct tetra_mac_state *tms, struct msgb *msg, unsigned int len)
         uint8_t mm_type = bits_to_uint(bits + 3, 4);
         const char *mm_name = tetra_get_mm_pdut_name(mm_type, 0);
 
-        char b[160];
         if (tms && tms->ssi) {
-            snprintf(b, sizeof(b),
-                     "ISSI=%u (0x%06X) MM type=0x%X (%s)",
-                     (unsigned)tms->ssi, (unsigned)tms->ssi,
-                     (unsigned)mm_type, mm_name);
+            // LA ophalen (optioneel)
+            int la = _this->osmotetradecoder.getLA(); // Zorg ervoor dat la echt wordt ingevuld
+
+            // Log MM type met echte ISSI
+            mm_logf_with_ctx(tms->ssi, la, "MM type=0x%X (%s) ISSI=%u (0x%06X)",
+                             (unsigned)mm_type, mm_name,
+                             (unsigned)tms->ssi, (unsigned)tms->ssi);
         } else {
-            snprintf(b, sizeof(b),
-                     "ISSI=UNKNOWN MM type=0x%X (%s)",
-                     (unsigned)mm_type, mm_name);
+            // Als ISSI onbekend is
+            mm_logf_with_ctx(0, -1, "MM type=0x%X (%s) ISSI=UNKNOWN",
+                             (unsigned)mm_type, mm_name);
         }
-        mm_log(b);
         break;
     }
 
     case TMLE_PDISC_CMCE:
-        /* optioneel */
+        // Optioneel logging, voeg hier toe als gewenst
         break;
 
     case TMLE_PDISC_SNDCP:
-        /* optioneel */
+        // Optioneel logging, voeg hier toe als gewenst
         break;
 
     default:
